23542ae7fbb85c5369f91f3a2e7252e510610a91
MahnoorSwati <mahnoorkhan_hallian@yahoo.com>
2017-01-16 00:24:35 +0500

    Calculator

---
+++ Calculator/src/pk/ciit/calc/Calculator.java
@@ -0,0 +1,28 @@
+package pk.ciit.calc;
+
+/**
+ * This is the best calculator in the world
+ * @author sajid
+ * @since 30/11/2015
+ */
+public class Calculator {
+	/**
+	 * Default constructor
+	 */
+	public Calculator() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	/**
+	 * This method calculates the sum of two numbers
+	 * @param num1 is the first number to be added
+	 * @param num2 is the second number to be added
+	 * @return the sum of both numbers
+	 */
+	public int sum(int num1, int num2){
+		return num1 + num2;
+	}
+	public int diff(int num1, int num2){
+		return num1 - num2;
+	}
+}
